b3e0d5efd8dc7482c696c6ae43bbc734e96df21a
Florian Schmidt <florian.schmidt.1994@icloud.com>
2016-06-13 14:53:21 +0200

    Update queue.cs

--- queue.cs
+++ queue.cs
@@ -6,25 +6,32 @@ class Program
 {
     static void Main()
     {
+        // Neue Queue erzeugen
         Queue<String> myQueue = new Queue<String>();
         
+        // "A" an die Queue anhängen
         myQueue.Enqueue("A");
         PrintQueue(myQueue);
         
+        // "F" an die Queue anhängen
         myQueue.Enqueue("F");
         PrintQueue(myQueue);
         
+        // "C" an die Queue anhängen
         myQueue.Enqueue("C");
         PrintQueue(myQueue);
         
+        // Vorderstes Element von der Queue entfernen
         String begin = myQueue.Dequeue();
         Console.Out.WriteLine("\nVom Anfang entfernt: "+begin);
         PrintQueue(myQueue);
         
+        // Vorderstes Element von der Queue entfernen
         begin = myQueue.Dequeue();
         Console.Out.WriteLine("\nVom Anfang entfernt: "+begin);
         PrintQueue(myQueue);
         
+        // "B" an die Queue anhängen
         myQueue.Enqueue("B");
         PrintQueue(myQueue);
         
